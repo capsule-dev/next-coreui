e4653dd708f53ceab470b9186b88a96c4430449b
Tanapat Sirisurt <tanapat.sir@ascendcorp.com>
2020-02-04 09:49:08 +0700

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 const withPlugins = require('next-compose-plugins');
 const withCSS = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
-// const plugins = [];
-// module.exports = withPlugins(plugins, {});
-module.exports = withSass(withCSS())
+const plugins = [withSass(withCSS({
+            webpack: function(config){
+                config.module.rules.push({
+                    test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+                    use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 100000,
+                        name: '[name].[ext]'
+                        }
+                    }
+                });
+                return config;
+            }
+        })
+    )
+    ];
+module.exports = withPlugins(plugins, {});
